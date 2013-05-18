d374284405f3f8bc8f2ef3176b5f074d26ad27d6
Steven Cummings <estebistec@gmail.com>
2013-05-17 22:44:09 -0500

    Update README & setup.py for v2.3.2

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ A sample working project can be found
 First, install the app:
 
 ```bash
-pip install django-twitter-bootstrap==2.3.1
+pip install django-twitter-bootstrap==2.3.2
 ```
 
 Then include it in your Django project.
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-twitter-bootstrap",
-    version="2.3.1",
+    version="2.3.2",
     packages=find_packages(),
     package_data={
         'twitter_bootstrap': [
