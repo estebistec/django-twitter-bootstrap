1ffec9222a07bafca484897d0b103f1d9bb87d35
Steven Cummings <estebistec@gmail.com>
2013-12-24 22:33:32 -0600

    Update demo_site for bootstrap 3.0.1
    * Also, remove nginx setup as we can now just run it with gunicorn+dj-static for a realistic production scenario

--- demo_site/public/urls.py
+++ demo_site/public/urls.py
@@ -11,10 +11,11 @@ from . import views
 
 urlpatterns = patterns('',
     (r'^$', RedirectView.as_view(url=reverse_lazy(u"home"))),
-    url(r'^index$', views.home, name=u"home"),
-    url(r'^components$', views.components, name=u"components"),
-    url(r'^css$', views.css, name=u"css"),
-    url(r'^customize$', views.customize, name=u"customize"),
-    url(r'^getting-started$', views.getting_started, name=u"getting-started"),
-    url(r'^javascript$', views.javascript, name=u"javascript"),
+    url(r'^index/$', views.home, name=u"home"),
+    url(r'^components/$', views.components, name=u"components"),
+    url(r'^css/$', views.css, name=u"css"),
+    url(r'^customize/$', views.customize, name=u"customize"),
+    url(r'^getting-started/$', views.getting_started, name=u"getting-started"),
+    url(r'^javascript/$', views.javascript, name=u"javascript"),
+    url(r'^about/$', views.about, name=u"about"),
 )
